78a0fd8a7f2e54476997f5bd6f8e0dd47ec1255e
Linfeng Zhou <luke.zlf@gmail.com>
2015-11-09 10:19:00 -0500

    add HW7

--- HW7/PUI2015_lz1335_hw7_1.sql
+++ HW7/PUI2015_lz1335_hw7_1.sql
@@ -3,7 +3,7 @@ start_station_id,
 start_station_name,
 end_station_id,
 end_station_name, 
-SUM(tripduration) AS trip_count
+COUNT(tripduration) AS trip_count
 
 FROM citibike
 
@@ -11,4 +11,10 @@ GROUP BY
 start_station_id,
 start_station_name,
 end_station_id,
-end_station_name;
+end_station_name
+
+ORDER BY 
+trip_count DESC
+
+LIMIT 5
+
--- HW7/PUI2015_lz1335_hw7_2.sql
+++ HW7/PUI2015_lz1335_hw7_2.sql
@@ -1,20 +1,25 @@
 SELECT start_station_id, start_station_name,  CDB_TransformToWebmercator(CDB_LatLng(
   start_station_latitude, 
   start_station_longitude)) 
-as the_geom_webmercator, 
-SUM(tripduration) AS trip_count 
+as start_station_location, 
+COUNT(tripduration) AS trip_count 
 
 FROM citibike
 
 WHERE ST_Dwithin(CDB_LatLng(
-  start_station_latitude,
-  start_station_longitude
+  end_station_latitude,
+  end_station_longitude
 )::geography,
        CDB_LatLng(40.7307602,-73.9974086)::geography,
            1000)
 
+and 
+extract(DOW FROM starttime) IN (0,6)
+
 GROUP BY
 start_station_id,
 start_station_name,
-start_station_latitude,
-start_station_longitude;
+start_station_location
+
+ORDER BY trip_count DESC
+
